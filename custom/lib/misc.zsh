4250b28084ec5be5ecd5a58292e2662d88c52c42
Keenahn Tiberius Jung <keenahn@users.noreply.github.com>
2019-01-26 21:02:55 -0800

    feat: comment out useless aliases

--- custom/lib/misc.zsh
+++ custom/lib/misc.zsh
@@ -22,15 +22,15 @@ env_default 'PAGER' 'less'
 env_default 'LESS' '-R'
 
 ## super user alias
-alias _='sudo'
-alias please='sudo'
+# alias _='sudo'
+# alias please='sudo'
 
 ## more intelligent acking for ubuntu users
-if which ack-grep &> /dev/null; then
-  alias afind='ack-grep -il'
-else
-  alias afind='ack -il'
-fi
+# if which ack-grep &> /dev/null; then
+#   alias afind='ack-grep -il'
+# else
+#   alias afind='ack -il'
+# fi
 
 # only define LC_CTYPE if undefined
 if [[ -z "$LC_CTYPE" && -z "$LC_ALL" ]]; then
